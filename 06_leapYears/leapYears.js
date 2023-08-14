9c31ddd440139d1fa06461d4e458ce2b618d20bc
MartinEskaros <martineskaros@gmail.com>
2023-08-13 22:57:38 -0400

    leap years function done, using if statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYEar = true;
 
-};
+
+if(year%4===0){ 
+    if(year %100 === 0){
+        if (year %400 !=0){
+         isLeapYEar=false;
+        }else{
+         isLeapYEar=true;
+        }
+    }else{
+        isLeapYEar=true;
+    }
+}else{
+    isLeapYEar=false;
+}
+return isLeapYEar;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
