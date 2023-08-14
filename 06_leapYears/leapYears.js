2b5a486a5898087060c70a9b8504ba13671902f5
MartinEskaros <martineskaros@gmail.com>
2023-08-13 23:05:12 -0400

    updated leap years function done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,21 +1,7 @@
 const leapYears = function(year) {
-    let isLeapYEar = true;
 
+return (year%4===0 && (year%100 !=0) || (year%400 ===0))
 
-if(year%4===0){ 
-    if(year %100 === 0){
-        if (year %400 !=0){
-         isLeapYEar=false;
-        }else{
-         isLeapYEar=true;
-        }
-    }else{
-        isLeapYEar=true;
-    }
-}else{
-    isLeapYEar=false;
-}
-return isLeapYEar;
 }
 
 // Do not edit below this line
