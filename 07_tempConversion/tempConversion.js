f672f62b59c2db380d53ac7fd89543a3896d852b
MartinEskaros <martineskaros@gmail.com>
2023-08-14 00:11:41 -0400

    both functions for converting done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahr) {
+  let resultInCelsius = (tempInFahr-32)*(5/9);
+  let roundedResult = (Math.round(resultInCelsius*10))/10;
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  let resultInFahr = (tempInCel*(9/5))+32;
+  let roundedResult = (Math.round(resultInFahr*10))/10;
+  return roundedResult;
 };
 
 // Do not edit below this line
