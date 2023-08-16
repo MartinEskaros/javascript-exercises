89b675bfec01aac96e79184bad068d688f8591aa
MartinEskaros <martineskaros@gmail.com>
2023-08-16 19:48:02 -0400

    Finished all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
+const add = function(num1,num2) {
+  result=num1+num2;
+  return result;
+};
+
+const subtract = function(num1,num2) {
+  result=num1-num2;
+  return result;
+};
+
+const sum = function(arr) {
+	let sum = arr.reduce((accumulator,currentValue) => accumulator+currentValue,0); // the 0 at the end sets the accumulator to 0
+  return sum;
+};
+
+const multiply = function(arr) {
+  let product = arr.reduce((accumulator,currentValue)=> accumulator*currentValue,1);
+  return product;
+};
+
+const power = function(num1,num2) {
+return Math.pow(num1,num2);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product=1;
+  for (let i=1;i<=num;i++){
+    product=product*i;
+  }
+  return product;
 };
 
 // Do not edit below this line
