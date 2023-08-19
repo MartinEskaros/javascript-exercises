6ea59f90284ac00013da57b7bf3c53c6d5ead8ea
MartinEskaros <martineskaros@gmail.com>
2023-08-18 21:55:43 -0400

    finished palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let i=0;
+    
+    let isPalindrome = true;
+    const wordPlain = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    let j = wordPlain.length -1;
+    while(i<=j){
+        
+        if(wordPlain.charAt(i).toLowerCase()!=wordPlain.charAt(j).toLowerCase()){
+            isPalindrome =false;
+            break;
+        }else{
+            i++;
+            j--;
+        }
+    }
+    return isPalindrome;
 
 };
 
