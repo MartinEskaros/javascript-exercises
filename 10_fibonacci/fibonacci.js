a3233e7c4f2cadd48ae612e7b3e9761cfcd9ade0
MartinEskaros <martineskaros@gmail.com>
2023-09-04 19:40:00 -0400

    Finished function that returns specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+if(n<0){
+    return "OOPS";
+}
+if(n<=1){
+    return parseInt(n);
+}
+return fibonacci(n-2)+fibonacci(n-1);
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
