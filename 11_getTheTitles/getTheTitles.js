755014ceaf36c382c8e546072023a233e21e7008
MartinEskaros <martineskaros@gmail.com>
2023-09-04 20:06:50 -0400

    returns array of book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map(function(book){    //map function will go through each element in array and then will apply the function we set to it. In this case book will represent the individual element
+        return book.title;              //in that array and then we access title using .title on each element in the array.
+    }); 
 };
 
+//alternatively we can use arrow functions
+// const getTheTitles = function(array) {
+//  return array.map((book)=> book.title);          //Here we specify that each element in the array and then use the arrow function to say we want to get .title from each element and return an array
+//                                                  //of only titles.
+//
+//
+//
 // Do not edit below this line
 module.exports = getTheTitles;
